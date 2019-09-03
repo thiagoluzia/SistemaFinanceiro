1e85be6842d3b7f4bbd1a055e6e0349b2608ee5a
Thiago Moura <thiago.mouraluzia@gmail.com>
2019-09-02 23:17:50 -0300

    Conclusão da BLL usuário, para funcionalidades futuras

---
+++ BLL/UsuarioBLL.cs
@@ -0,0 +1,54 @@
+﻿using DAL;
+using DTO;
+using System.Data;
+
+namespace BLL
+{
+    class UsuarioBLL
+    {
+        AcessoBancoDados banco = new AcessoBancoDados();
+        DataTable data = new DataTable();
+
+        #region Inserir
+        public void Inserir(UsuarioDTO dto)
+        {
+            banco = new AcessoBancoDados();
+            banco.Conectar();
+            string comando = "INSERT INTO usuario(nome, sobrenome) VALUES('" + dto.Nome + "', '" + dto.Sobrenome + "'";
+            banco.ExecutarComandoSql(comando);
+        }
+        #endregion
+
+        #region Exibir
+        public DataTable Exibir()
+        {
+            banco = new AcessoBancoDados();
+            banco.Conectar();
+            string comando = "SELECT * FROM usuario";
+            data = new DataTable();
+            data = banco.RetdataTable(comando);
+            return data;
+        }
+        #endregion
+
+        #region Atualizar
+        public void Atualizar(UsuarioDTO dto)
+        {
+            banco = new AcessoBancoDados();
+            banco.Conectar();
+            string comando = "UPDATE usuario SET nome = '" + dto.Nome + "', sobrenome = '" + dto.Sobrenome + "' WHERE id = '" + dto.Id + "'";
+            banco.ExecutarComandoSql(comando);
+        }
+        #endregion
+
+        #region Excluir
+        public void Excluir(UsuarioDTO dto)
+        {
+            banco = new AcessoBancoDados();
+            banco.Conectar();
+            string comando = "DELETE FROM usuario WHERE id = '" + dto.Id + "'";
+            banco.ExecutarComandoSql(comando);
+        }
+        #endregion
+    }
+}
